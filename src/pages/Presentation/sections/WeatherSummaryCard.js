a0f9b53f31ced2adb1787944114d4105f48bf206
Makeskumar <makeshkumar5882@gmail.com>
2025-06-28 21:15:48 +0530

    part-12| box shadow  is done

--- src/pages/Presentation/sections/WeatherSummaryCard.js
+++ src/pages/Presentation/sections/WeatherSummaryCard.js
@@ -180,7 +180,7 @@ function WeatherSummaryCard({
           width: "100%",
           p: 2,
           borderRadius: 2,
-          backgroundColor: "rgba(255, 255, 255, 0.4)",
+          backgroundColor: "rgba(0, 0, 0, 0.08)",
           boxShadow: "0 2px 8px rgba(0,0,0,0.1)",
         }}
       >
